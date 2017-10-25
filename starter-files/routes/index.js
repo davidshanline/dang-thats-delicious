bf912df9b496bb45a6dccb5327df4b524e860898
davidshanline <davidshanline+programming@gmail.com>
2017-10-25 11:30:38 -0400

    Initial commit; all work through lesson 22

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore) //createStore needs to be wrapped in catchErrors because it as an async/await function
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;
