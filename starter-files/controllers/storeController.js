9b009bee347bb4fdc17fc21d328fb637a14fbde8
davidshanline <davidshanline+programming@gmail.com>
2017-10-27 08:55:47 -0400

    Lesson 31
    All updates through lesson 31

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -43,6 +43,7 @@ exports.resize = async(req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
@@ -54,11 +55,17 @@ exports.getStores = async (req, res) => {
   res.render('stores', {title: 'Stores', stores});
 };
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)){
+    throw Error('You must own a store in order to edit it');
+  }
+}
+
 exports.editStore = async (req, res) => {
   //1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id});
   //2. confirm they are the owner of the store
-  // TODO
+  confirmOwner(store, req.user);
   //3. Render the out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store});
 };
@@ -77,7 +84,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug});
+  const store = await Store.findOne({ slug: req.params.slug}).populate('author');
   if (!store) return next();
   res.render('store', {store, title: store.name});
 };
@@ -89,4 +96,23 @@ exports.getStoresByTag = async (req, res) => {
   const storesPromise = Store.find({ tags: tagQuery});
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
   res.render('tags', { tags, title: 'Tags', tag, stores});
+}
+
+exports.searchStores = async (req, res) => {
+  const stores = await Store
+  // first fined stores that match
+  .find({
+    $text: {
+      $search: req.query.q
+    }
+  }, {
+    score: { $meta: 'textScore' }
+  })
+  // then sort them
+  .sort({
+    score: { $meta: 'textScore' }
+  })
+  // limit to only 5
+  .limit(5);
+  res.json(stores);
 }
